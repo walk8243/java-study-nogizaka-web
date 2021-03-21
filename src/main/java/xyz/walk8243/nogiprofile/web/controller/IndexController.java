706a6230f9d907e3595dcf84d58f2689fef8ac95
walk8243 <walk8243@gmail.com>
2021-03-22 02:46:24 +0900

    BE APIにリクエストを送信してその結果をページ表示

--- src/main/java/xyz/walk8243/nogiprofile/web/controller/IndexController.java
+++ src/main/java/xyz/walk8243/nogiprofile/web/controller/IndexController.java
@@ -1,13 +1,20 @@
 package xyz.walk8243.nogiprofile.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import xyz.walk8243.nogiprofile.web.service.IndexService;
+
 @Controller
 public class IndexController {
+	@Autowired
+	IndexService indexService;
+
 	@GetMapping
 	public String index(Model model) {
+		model.addAttribute("be", indexService.getResponse());
 		return "index";
 	}
 }
