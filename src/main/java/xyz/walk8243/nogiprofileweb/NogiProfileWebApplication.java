7dfd17f3fa506fa429dd692475a63c9d57abba9d
walk8243 <walk8243@gmail.com>
2021-03-21 20:32:31 +0900

    Spring Initializr

---
+++ src/main/java/xyz/walk8243/nogiprofileweb/NogiProfileWebApplication.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.nogiprofileweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogiProfileWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NogiProfileWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/walk8243/nogiprofileweb/NogiProfileWebApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.nogiprofileweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NogiProfileWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
