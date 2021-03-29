e6362461f169786e8702fb1d4941424f8e03644e
walk8243 <walk8243@gmail.com>
2021-03-30 01:16:32 +0900

    ディスクに含まれる曲順を表示

--- src/main/java/xyz/walk8243/nogiprofile/web/model/DiscographyDetail.java
+++ src/main/java/xyz/walk8243/nogiprofile/web/model/DiscographyDetail.java
@@ -3,12 +3,23 @@ package xyz.walk8243.nogiprofile.web.model;
 import java.util.List;
 
 public class DiscographyDetail extends Discography {
-	private List<Song> songs;
+	private List<SongOnDisc> songs;
 
-	public List<Song> getSongs() {
+	public List<SongOnDisc> getSongs() {
 		return songs;
 	}
-	public void setSongs(List<Song> songs) {
+	public void setSongs(List<SongOnDisc> songs) {
 		this.songs = songs;
+	}
+
+	static class SongOnDisc extends Song {
+		private Integer order;
+
+		public Integer getOrder() {
+			return order;
+		}
+		public void setOrder(Integer order) {
+			this.order = order;
+		}
 	}
 }
