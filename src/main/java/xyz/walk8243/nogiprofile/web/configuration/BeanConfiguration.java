a236edad6fbc7f312514ed22c63c685d82df4090
walk8243 <walk8243@gmail.com>
2021-04-28 20:51:36 +0900

    ちょっと構成を変更

--- src/main/java/xyz/walk8243/nogiprofile/web/configuration/BeanConfiguration.java
+++ src/main/java/xyz/walk8243/nogiprofile/web/configuration/BeanConfiguration.java
@@ -1,9 +1,11 @@
-package xyz.walk8243.nogiprofile.web;
+package xyz.walk8243.nogiprofile.web.configuration;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import xyz.walk8243.nogiprofile.web.AppProperties;
 
 @Configuration
 @EnableConfigurationProperties(AppProperties.class)
--- src/main/java/xyz/walk8243/nogiprofile/web/repository/NogizakaApiRepositoryImpl.java
+++ src/main/java/xyz/walk8243/nogiprofile/web/repository/NogizakaApiRepositoryImpl.java
@@ -3,10 +3,10 @@ package xyz.walk8243.nogiprofile.web.repository;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.Getter;
 import xyz.walk8243.nogiprofile.web.AppProperties;
 import xyz.walk8243.nogiprofile.web.model.Discography;
 import xyz.walk8243.nogiprofile.web.model.DiscographyDetail;
@@ -15,33 +15,34 @@ import xyz.walk8243.nogiprofile.web.model.SongDetail;
 
 @Repository
 public class NogizakaApiRepositoryImpl implements NogizakaApiRepository {
-	@Autowired
-	RestTemplate restTemplate;
+	private RestTemplate restTemplate;
+	@Getter private String baseUrl;
 
-	@Autowired
-	AppProperties appProperties;
+	public NogizakaApiRepositoryImpl(
+		RestTemplate restTemplate,
+		AppProperties appProperties
+	) {
+		this.restTemplate = restTemplate;
+		this.baseUrl = appProperties.getBackend().getUrl();
+	}
 
 	public SongDetail getSong(Integer songId) {
-		SongDetail song = restTemplate.getForObject(getBaseUrl() + "/song/discs/" + songId.toString(), SongDetail.class);
+		SongDetail song = restTemplate.getForObject(baseUrl + "/song/discs/" + songId.toString(), SongDetail.class);
 		return song;
 	}
 
 	public List<Song> getSongAll() {
-		Song[] songs = restTemplate.getForObject(getBaseUrl() + "/song/all", Song[].class);
+		Song[] songs = restTemplate.getForObject(baseUrl + "/song/all", Song[].class);
 		return Arrays.asList(songs);
 	}
 
 	public DiscographyDetail getDiscography(Integer discId) {
-		DiscographyDetail disc = restTemplate.getForObject(getBaseUrl() + "/disc/songs/" + discId.toString(), DiscographyDetail.class);
+		DiscographyDetail disc = restTemplate.getForObject(baseUrl + "/disc/songs/" + discId.toString(), DiscographyDetail.class);
 		return disc;
 	}
 
 	public List<Discography> getDiscographyAll() {
-		Discography[] discs = restTemplate.getForObject(getBaseUrl() + "/disc/all", Discography[].class);
+		Discography[] discs = restTemplate.getForObject(baseUrl + "/disc/all", Discography[].class);
 		return Arrays.asList(discs);
-	}
-
-	private String getBaseUrl() {
-		return appProperties.getBackend().getUrl();
 	}
 }
